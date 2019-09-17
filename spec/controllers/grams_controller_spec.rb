c12151643b2adc58bb4bc6e9fc776a9482caeee5
Gregory Eglen <eglen.g@gmail.com>
2019-09-17 03:00:07 +0000

    Added simple form gem and new grams view

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+  describe "grams#new action" do
+    it "should successfully show the new form" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "grams#create action" do
+    it "should seccessfully create a new gram in our database" do
+      post :create, params: { gram: { message: 'Hello!' } }
+      expect(response).to redirect_to root_path
+
+      gram = Gram.last
+      expext(gram.message).to eq("Hello!")
+    end
+  end
 end
